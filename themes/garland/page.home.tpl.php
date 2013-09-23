4cb35e8adcc4c75f802b041caa3cd0bc49880665
Jack <mmcdonaldm@gmail.com>
2013-09-23 23:57:49 +0800

    Add profile page.

--- themes/garland/page.home.tpl.php
+++ themes/garland/page.home.tpl.php
@@ -1,48 +1,7 @@
-<div class="navbar navbar-inverse navbar-fixed-top" >
-            <div class="navbar-inner">
-                <div class="container">
-                  
-                    <a class="brand" href="/">思君堂</a>
-                    <div class="nav-collapse collapse">
-                        <ul class="nav">
-                            <li class="active">
-                                	<a>农历七月二十九   [杨公忌日，大事勿用]</a>
-                            </li>                            
-                        <li>
-                        </li>
-                       </ul>
-                        <div id="div_login" class="pull-right">
-                            	<?php
-									global $user;
-								?>
-								<?php if($user && $user->uid != 0): ?>
-									<ul class="nav">
-										<li>
-                          		      		<a href="/user"><?php echo $user->name;?></a>
-                          		      	</li>
-                          		      	<li class="logout last">
-                          		      		<a href="/user/logout">登出</a>
-                          		      	</li>
-                          		     </ul>
-                          		<?php else:?>
-		                        	<form class="navbar-form" action="/user" method="post" id="user-login" accept-charset="UTF-8">
-									  <input type="text" class="input-small required" id="edit-name" name="name" value="" placeholder="用户名">
-									  <input type="password" id="edit-pass" name="pass" class="input-small required" placeholder="密码"/>
-									  <input type="hidden" name="form_id" value="user_login"/>
-									  <button type="submit" class="btn" id="edit-submit" name="op">登录</button>
-									  <a href="/user/register" type="submit" class="btn">注册</a>
-									</form>
-            					<?php endif; ?>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        
          <div class="container">
 			  <div class="row">
 				  <div class="span12 home_container0">
-				  <?php
+				 	<?php
 					  $path = drupal_get_path('theme', 'garland');
 					?>
 					<div class="row">
@@ -84,27 +43,3 @@
 				  	</div>
 					</div>
 		  	</div>
-<div class="navbar navbar-fixed-bottom">
- 	<div class="navbar-inner">
-                <div class="container">
-                    <div class="nav-collapse collapse pull-right">
-                        <ul class="nav">
-                            <li class="active">
-                                	<a>帮助</a>
-                            </li>                            
-                            <li class="active">
-                                	<a>实例</a>
-                            </li>                            
-                            <li class="active">
-                                	<a>在线客服</a>
-                            </li>                            
-                            <li class="active">
-                                	<a>关于我们</a>
-                            </li>                            
-                        <li>
-                        </li>
-                       </ul>
-                    </div>
-                </div>
-            </div>
-</div>
---
+++ themes/garland/page.user.tpl.php
@@ -0,0 +1,199 @@
+<?php
+$path = drupal_get_path ( 'theme', 'garland' );
+?>
+<div class="container">
+	<div class="container-fluid">
+		<div class="row-fluid">
+			<div class="span7">
+				<div class="row-fluid">
+					<div class="span12"></div>
+					<div class="span12 section_padding">
+						<div class="row-fluid">
+							<div class="span3">
+								<img alt="pic"
+									src="/<?php echo $path ?> /memoryimg/u5_normal.png"
+									class=" hero_img">
+								<p style="text-align: center;">
+									<span>张国荣</span>
+								</p>
+							</div>
+							<div class="span3">
+								<img alt="pic"
+									src="/<?php echo $path ?> /memoryimg/u5_normal.png"
+									class="img-rounded hero_img">
+								<p style="text-align: center;">
+									<span>张国荣</span>
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="span12">
+						<p>
+							<span class="section_title_p" style="width:190;">礼品</span>
+						</p>
+						<p style="text-align: right;">
+							<span>思君币：1000点 </span><a>兑换思君币</a>
+						</p>
+					</div>
+					<div class="span12 section_padding">
+						<div class="row-fluid">
+							<div class="span2">
+								<p class="left_name_align">
+									<img alt="pic"
+										src="/<?php echo $path ?> /memoryimg/u18_normal.png"
+										class="img-rounded, home_img">
+								</p>
+								<p class="left_name_align">
+									<span>长明烛</span>
+								</p>
+								<p class="left_name_align">
+									<span>数量 : 3</span>
+								</p>
+							</div>
+							<div class="span2">
+								<p class="left_name_align">
+									<img alt="pic"
+										src="/<?php echo $path ?> /memoryimg/u20_normal.png"
+										class="img-rounded, home_img">
+								</p>
+								<p class="left_name_align">
+									<span>万年菊</span>
+								</p>
+								<p class="left_name_align">
+									<span>数量 : 3</span>
+								</p>
+							</div>
+							<div class="span2">
+								<p class="left_name_align">
+									<img alt="pic"
+										src="/<?php echo $path ?> /memoryimg/u22_normal.png"
+										class="img-rounded, home_img">
+								</p>
+								<p class="left_name_align">
+									<span>鞭炮</span>
+								</p>
+								<p class="left_name_align">
+									<span>数量 : 3</span>
+								</p>
+							</div>
+							<div class="span2">
+								<p class="left_name_align">
+									<img alt="pic"
+										src="/<?php echo $path ?> /memoryimg/u24_normal.jpg"
+										class="img-rounded, home_img">
+								</p>
+								<p class="left_name_align">
+									<span>金元宝</span>
+								</p>
+								<p  class="left_name_align">
+									<span>数量 : 3</span>
+								</p>
+							</div>
+							<div class="span2">
+								<p class="left_name_align">
+									<img alt="pic"
+										src="/<?php echo $path ?> /memoryimg/u26_normal.png"
+										class="img-rounded, home_img">
+								</p>
+								<p class="left_name_align">
+									<span>银元宝</span>
+								</p>
+								<p class="left_name_align">
+									<span>数量 : 3</span>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="span4 offset1">
+				<div class="row-fluid">
+					<div class="span12 section_padding">
+						<span class="section_title_p">邀请</span>
+					</div>
+					<div class="span12">
+						<ul>
+							<li>
+								<p>
+									<span>2013-09-07 18:09 <a>陈某女</a> 邀请你加入<a>翻江鼠</a>的亲友团
+									</span>
+									<button class="btn-link">接受</button>
+									<button class="btn-link">拒绝</button>
+								</p>
+							</li>
+							<li>
+								<p>
+									<span>2013-09-07 18:09 <a>陈某女</a> 邀请你加入<a>翻江鼠</a>的亲友团
+									</span>
+									<button class="btn-link">接受</button>
+									<button class="btn-link">拒绝</button>
+								</p>
+							</li>
+							<li>
+								<p>
+									<span>2013-09-07 18:09 <a>陈某女</a> 邀请你加入<a>翻江鼠</a>的亲友团
+									</span>
+									<button class="btn-link">接受</button>
+									<button class="btn-link">拒绝</button>
+								</p>
+							</li>
+							<li>
+								<p>
+									<span>2013-09-07 18:09 <a>陈某女</a> 邀请你加入<a>翻江鼠</a>的亲友团
+									</span>
+									<button class="btn-link">接受</button>
+									<button class="btn-link">拒绝</button>
+								</p>
+							</li>
+							<li>
+								<p>
+									<span>2013-09-07 18:09 <a>陈某女</a> 邀请你加入<a>翻江鼠</a>的亲友团
+									</span>
+									<button class="btn-link">接受</button>
+									<button class="btn-link">拒绝</button>
+								</p>
+							</li>
+							<li>
+								<p>
+									<span>2013-09-07 18:09 <a>陈某女</a> 邀请你加入<a>翻江鼠</a>的亲友团
+									</span>
+									<button class="btn-link">接受</button>
+									<button class="btn-link">拒绝</button>
+								</p>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span12 section_title_p">
+						<span class="section_title_p">访客日志</span>
+					</div>
+					<div class="span12">
+						<ul>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+							<li><span>2013-09-07 18:09 <a>陈某女 </a>浏览了 <a>张国荣</a> 的相册
+							</span></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
